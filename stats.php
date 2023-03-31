a85b30b76cecb3916f3ed9b1c244143922592e86
Belzeus30 <radekraisiglgmail.com>
2023-03-31 19:28:28 +0200

    asdadsadasfasfasfasfsaf

--- stats.php
+++ stats.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel ="stylesheet" href="css/stats.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Statistiky</title>
-</head>
+<?php
+include "includes/head.php";
+?>
 <body>
 
 <?php 
