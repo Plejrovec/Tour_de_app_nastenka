a572f4ab2a4ba2323ee844a38a57e2877b74ce5c
Belzeus30 <radekraisiglgmail.com>
2023-03-31 18:35:29 +0200

    stepan je idiot

--- index.php
+++ index.php
@@ -1,83 +1,65 @@
 <?php
-require_once 'config/init.php';
 include "includes/head.php";
 ?>
 <body>
-    <form class="form" action="/action_page.php" id=myForm>
-        <div class=sticky-note>
-            <button class=close onclick="closeForm()">x</button>
-            <textarea class="text" rows="14" cols="10" wrap="soft" maxlength="120"
-                style="overflow:hidden; resize:none;"></textarea>
-            <textarea class="sign" rows="3" cols="10" wrap="soft" maxlength="20"
-                style="overflow:hidden; resize:none;"></textarea>
-            <button class="button2">Submit</button>
-        </div>
+
         
 
 </div>
+<?php 
+require "models/database.php";
+require "models/note.php";
+require "config/init.php";
 
+$result = $n->read();
+    // get row count
+    $num = $result->rowCount();
+
+    // Check if any posts
+    if($num > 0) {
+        // Record array 
+        $notes = array();
+        $notes['data'] = array();
+        }
+?>
 <button class="button" onclick="openForm()">+</button>
 <ul>
+    <?php while($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
   <li>
     <a href="#">
-      <p>Title #1</p>
-      <p>Text Content #1</p>
+      <h2><?=$row["text"]?></h2>
+      <p><?=$row["signature"]?></p>
     </a>
   </li>
-  <li>
-    <a href="#">
-      <p>Title #2</p>
-      <p>Text Content #2</p>
-    </a>
-  </li>
-  <li>
-    <a href="#">
-      <p>Title #1</p>
-      <p>Text Content #1</p>
-    </a>
-  </li>
-  <li>
-    <a href="#">
-      <p>Title #2</p>
-      <p>Text Content #2</p>
-    </a>
-  </li>
-    <li>
-    <a href="#">
-      <p>Title #1</p>
-      <p>Text Content #1</p>
-    </a>
-  </li>
-  <li>
-    <a href="#">
-      <p>Title #2</p>
-      <p>Text Content #2</p>
-    </a>
-  </li>
-
-
-</ul>    
-    <form class="form" action="/action_page.php" id=myForm>
+  <?php endwhile?>
+</ul>
+<button class="button" onclick="openForm()">+</button>
+    
+    <form class="form" action="/parsers/savenote.php" method="post" id=myForm>
         <div class=sticky-note>
             <button class=close onclick="closeForm()">x</button>
-            <textarea class="text" rows="14" cols="10" wrap="soft" maxlength="120"
+            <textarea name="text" class="text" rows="14" cols="10" wrap="soft" maxlength="120"
                 style="overflow:hidden; resize:none;"></textarea>
-            <textarea class="sign" rows="3" cols="10" wrap="soft" maxlength="20"
+            <textarea name = "sign" class="sign" rows="3" cols="10" wrap="soft" maxlength="20"
                 style="overflow:hidden; resize:none;"></textarea>
             <button class="button2">Submit</button>
         </div>
 
     </form>
-    <form class="form" action="/action_page.php" id=myForm1>
+    <form class="form" action="" method="post" id=EditForm1>
         <div class=sticky-note>
-            <textarea class="text" rows="14" cols="10" wrap="soft" maxlength="120"
+            <textarea name="text" class="text" rows="14" cols="10" wrap="soft" maxlength="120"
                 style="overflow:hidden; resize:none;"></textarea>
-            <textarea class="sign" rows="3" cols="10" wrap="soft" maxlength="20"
+            <textarea name = "sign" class="sign" rows="3" cols="10" wrap="soft" maxlength="20"
                 style="overflow:hidden; resize:none;"></textarea>
             <input class="button2" value="Submit">
         </div>
 
     </form>
+    
+
+    
+    
 
     <script>
         function openForm() {
